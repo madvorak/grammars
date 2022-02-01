81b79168cef547170d0bbcd472dd9b979fa95c70
madvorak <dvorakmartinbridge@seznam.cz>
2022-02-01 17:07:23 +0100

    inductive derives

--- src/morerules.lean
+++ src/morerules.lean
@@ -1,4 +1,3 @@
-import data.list
 import onerule
 
 
@@ -8,13 +7,96 @@ def ruleset := finset production_rule
 
 def transformed (rules : ruleset) (oldWord newWord : string) : Prop :=
 ∃ r ∈ rules.val, transforms r oldWord newWord
-/-
-def derives (rules : ruleset) (startWord endWord : string) : Prop :=
-startWord = endWord ∨ (∃ intermediate : string,
-  transformed rules startWord intermediate ∧ derives rules intermediate endWord)
 
-inductive derives (rules : ruleset) (startWord endWord : string) : Prop
-| step_base : startWord = endWord → derives
-| step_indu : (∃ intermediate : string,
-    transformed rules startWord intermediate ∧ derives rules intermediate endWord) → derives
--/
+inductive derives (rules : ruleset) : string → string → Prop
+| step_base (word : string) : derives word word
+| step_indu (startWord intermediate endWord : string) :
+    transformed rules startWord intermediate → derives intermediate endWord → 
+      derives startWord endWord
+
+
+
+-- Demonstrations
+
+def abb : ruleset := finset.mk [a_bb] (by simp)
+
+example : derives abb "vana" "vbbnbb" :=
+begin
+  apply derives.step_indu "vana" "vbbna" "vbbnbb",
+  {
+    use a_bb,
+    use "v",
+    use "na",
+    finish,
+  },
+  apply derives.step_indu "vbbna" "vbbnbb" "vbbnbb",
+  {
+    use a_bb,
+    use "vbbn",
+    use "",
+    finish,
+  },
+  apply derives.step_base,
+end
+
+
+def cb_cac := production_rule.mk "cb" "cac"
+
+def tworules : ruleset := finset.mk [a_bb, cb_cac] sorry
+
+example : derives tworules "ca" "cacbcac" :=
+begin
+  have omg : cb_cac ∈ tworules.val,
+  {
+    unfold tworules,
+    simp,
+  },
+
+  fconstructor,
+  use "cbb",
+  {
+    use a_bb,
+    use "c",
+    use "",
+    finish,
+  },
+  fconstructor,
+  use "cacb",
+  {
+    use cb_cac,
+    split,
+      exact omg,
+    use "",
+    use "b",
+    finish,
+  },
+  fconstructor,
+  use "cacac", 
+  {
+    use cb_cac,
+    split,
+      exact omg,
+    use "ca",
+    use "",
+    finish,
+  },
+  fconstructor,
+  use "cbbcac",
+  {
+    use a_bb,
+    use "c",
+    use "cac",
+    finish,
+  },
+  fconstructor,
+  use "cacbcac", 
+  {
+    use cb_cac,
+    split,
+      exact omg,
+    use "",
+    use "bcac",
+    finish,
+  },
+  fconstructor,
+end
--- src/onerule.lean
+++ src/onerule.lean
@@ -10,6 +10,7 @@ def transforms (rule : production_rule) (oldWord newWord : string) : Prop :=
 ∃ v w : string, v ++ rule.input ++ w = oldWord ∧ v ++ rule.output ++ w = newWord
 
 
+
 -- Demonstrations
 
 def a_bb := production_rule.mk "a" "bb"
@@ -51,8 +52,8 @@ begin
   unfold transforms at assum,
   --rw a_bb at assum,
   have lit_in : a_bb.input = "a", refl,
-  have lit_out : a_bb.output = "bb", refl,
   rw lit_in at assum,
+  have lit_out : a_bb.output = "bb", refl,
   rw lit_out at assum,
   cases assum with v assum_,
   cases assum_ with w assum__,
@@ -64,10 +65,7 @@ begin
   have right_side : w = "ck",
   {
     rw left_side at hyp_before,
-    have foo: "l" ++ "a" = "la", refl,
-    rw foo at hyp_before,
-    have bar: "lack" = "la" ++ "ck", refl,
-    rw bar at hyp_before,
+    change "la" ++ w = "la" ++ "ck" at hyp_before,
     sorry,
     --exact list.append_inj_right hyp_before id,
   },
@@ -77,3 +75,7 @@ begin
   rw concaten at hyp_after,
   finish,
 end
+
+
+def asdf : list char := ['h', 'e', 'l', 'l', 'o']
+#print asdf
