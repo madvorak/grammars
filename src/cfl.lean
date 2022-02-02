1144835541c9e9823ffa53cd3474a207c9c546bb
madvorak <dvorakmartinbridge@seznam.cz>
2022-02-02 19:40:53 +0100

    this syntax is awful

--- src/cfl.lean
+++ src/cfl.lean
@@ -1,32 +1,51 @@
 import tactic
 import logic.relation
 
-
 -- Definitions
 
 inductive symbol
-| a
-| b
-| c
-| S
-| T
+| _a
+| _b
+| _c
+| _S
+| _T
 
-def is_nonterminal (x : symbol) : Prop := x = symbol.S ∨ x = symbol.T
+def is_terminal (x : symbol) : Prop := x ∈ [symbol._a, symbol._b, symbol._c]
 
-def nonterminal := {x : symbol // is_nonterminal x}
+def terminal := {x : symbol // is_terminal x}
 
-def terminal := {x : symbol // ¬ is_nonterminal x}
+def nonterminal := {x : symbol // ¬ is_terminal x}
 
-def nS : nonterminal := subtype.mk symbol.S
+def a : terminal := subtype.mk symbol._a
 begin
-  left,
-  refl,
+  unfold is_terminal,
+  finish,
 end
 
-def nT : nonterminal := subtype.mk symbol.T
+def b : terminal := subtype.mk symbol._b
 begin
-  right,
-  refl,
+  unfold is_terminal,
+  finish,
+end
+
+def c : terminal := subtype.mk symbol._c
+begin
+  unfold is_terminal,
+  finish,
+end
+
+def S : nonterminal := subtype.mk symbol._S
+begin
+  intro h,
+  unfold is_terminal at h,
+  finish,
+end
+
+def T : nonterminal := subtype.mk symbol._T
+begin
+  intro h,
+  unfold is_terminal at h,
+  finish,
 end
 
 structure CFgrammar :=
@@ -44,57 +63,57 @@ cfl_derives gr [subtype.val gr.initial] (list.map subtype.val word)
 
 -- Demonstrations
 
-def gramatika := CFgrammar.mk nS [
-  (nS, [symbol.a, symbol.S, symbol.c]),
-  (nS, [symbol.T]),
-  (nT, [symbol.b, symbol.T, symbol.c]),
-  (nT, []) ]
+def gramatika := CFgrammar.mk S [
+  (S, [symbol._a, symbol._S, symbol._c]),
+  (S, [symbol._T]),
+  (T, [symbol._b, symbol._T, symbol._c]),
+  (T, []) ]
 
-example : cfl_tranforms gramatika [symbol.b, symbol.S, symbol.b, symbol.a]
-                        [symbol.b, symbol.a, symbol.S, symbol.c, symbol.b, symbol.a] :=
+example : cfl_tranforms gramatika [symbol._b, symbol._S, symbol._b, symbol._a]
+                       [symbol._b, symbol._a, symbol._S, symbol._c, symbol._b, symbol._a] :=
 begin
   unfold gramatika,
-  use (nS, [symbol.a, symbol.S, symbol.c]),
+  use (S, [symbol._a, symbol._S, symbol._c]),
     simp,
   fconstructor,
-    exact [symbol.b],
+    exact [symbol._b],
   fconstructor,
-    exact [symbol.b, symbol.a],
+    exact [symbol._b, symbol._a],
   finish,
 end
 
-example : cfl_generates gramatika [subtype.mk symbol.a sorry, subtype.mk symbol.c sorry] :=
+example : cfl_generates gramatika [a, c] :=
 begin
-  have step_1 : cfl_tranforms gramatika [symbol.S] [symbol.a, symbol.S, symbol.c],
+  have step_1 : cfl_tranforms gramatika [symbol._S] [symbol._a, symbol._S, symbol._c],
   {
     unfold gramatika,
-    use (nS, [symbol.a, symbol.S, symbol.c]),
+    use (S, [symbol._a, symbol._S, symbol._c]),
     simp,
     use [[],[]],
     finish,
   },
-  have step_2 : cfl_tranforms gramatika [symbol.a, symbol.S, symbol.c] [symbol.a, symbol.T, symbol.c],
+  have step_2 : cfl_tranforms gramatika [symbol._a, symbol._S, symbol._c] [symbol._a, symbol._T, symbol._c],
   {
     unfold gramatika,
-    use (nS, [symbol.T]),
+    use (S, [symbol._T]),
     simp,
-    use [[symbol.a], [symbol.c]],
+    use [[symbol._a], [symbol._c]],
     finish,
   },
-  have step_3 : cfl_tranforms gramatika [symbol.a, symbol.T, symbol.c] [symbol.a, symbol.c],
+  have step_3 : cfl_tranforms gramatika [symbol._a, symbol._T, symbol._c] [symbol._a, symbol._c],
   {
     unfold gramatika,
-    use (nT, []),
+    use (T, []),
     simp,
-    use [[symbol.a], [symbol.c]],
+    use [[symbol._a], [symbol._c]],
     finish,
   },
   
-  have composed : cfl_derives gramatika [symbol.S] [symbol.a, symbol.c],
+  have composed : cfl_derives gramatika [symbol._S] [symbol._a, symbol._c],
   {
     --simp [step_1, step_2, step_3],
     fconstructor,
-      exact [symbol.a, symbol.S, symbol.c],
+      exact [symbol._a, symbol._S, symbol._c],
     --simp [step_1],
     sorry,
     sorry,
