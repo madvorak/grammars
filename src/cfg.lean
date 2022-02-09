b0be25c218962f4522a2673c38ef9d607ed5340f
madvorak <dvorakmartinbridge@seznam.cz>
2022-02-09 14:47:44 +0100

    shortening the new code

--- src/cfg.lean
+++ src/cfg.lean
@@ -1,5 +1,5 @@
 import logic.relation
-import tactic
+import data.fintype.basic
 import computability.language
 
 
@@ -7,47 +7,23 @@ inductive symbol (τ : Type) (μ : Type) [fintype τ] [fintype μ]
 | terminal : τ → symbol
 | nonterminal : μ →  symbol
 
-#check symbol
-
-#check list (ℕ × (list char))
-
-def aa : (fin 5) := 2
-def bb : (fin 7) := 6
-
-#check symbol (fin 5) (fin 7)
-
-def tt := symbol (fin 5) (fin 7)
-
-def tta : tt := symbol.terminal aa
-def ttb : tt := symbol.nonterminal bb
-
-#check tta
-#check ttb
-
-def parek : ((fin 5) × (list tt)) := (aa, [tta, ttb])
-
-#check parek
-#print parek
-
 structure CF_grammar (terminal : Type) (nonterminal : Type)
   [fintype terminal] [fintype nonterminal] :=
 (initial : nonterminal)
 (rules : list (nonterminal × (list (symbol terminal nonterminal))))
 
 
-def CF_transforms {T N : Type} [fintype T] [fintype N] 
-  (gr : CF_grammar T N) (oldWord newWord : list (symbol T N)) : Prop :=
-∃ r ∈ gr.rules, ∃ v w : list (symbol T N), 
+variables {T N : Type} [fintype T] [fintype N] (g : CF_grammar T N)
+
+def CF_transforms (oldWord newWord : list (symbol T N)) : Prop :=
+∃ r ∈ g.rules, ∃ v w : list (symbol T N), 
   oldWord = v ++ [symbol.nonterminal (prod.fst r)] ++ w ∧ newWord = v ++ (prod.snd r) ++ w
 
-def CF_derives {T N : Type} [fintype T] [fintype N] 
-  (gr : CF_grammar T N) : list (symbol T N) → list (symbol T N) → Prop :=
-relation.refl_trans_gen (CF_transforms gr)
+def CF_derives : list (symbol T N) → list (symbol T N) → Prop :=
+relation.refl_trans_gen (CF_transforms g)
 
-def CF_generates {T N : Type} [fintype T] [fintype N]
-  (gr : CF_grammar T N) (word : list (symbol T N)) : Prop :=
-CF_derives gr [symbol.nonterminal gr.initial] word
+def CF_generates (word : list (symbol T N)) : Prop :=
+CF_derives g [symbol.nonterminal g.initial] word
 
-def CF_language {T N : Type} [fintype T] [fintype N]
-  (gr : CF_grammar T N) : language (symbol T N) :=
-CF_generates gr
+def CF_language : language (symbol T N) :=
+CF_generates g
