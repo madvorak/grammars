3bd13c07c630d237eb019fef3f4313b6a942c85c
madvorak <dvorakmartinbridge@seznam.cz>
2022-02-15 19:43:32 +0100

    CFL closure union: second inclusion / first case done

--- src/cfgClosure.lean
+++ src/cfgClosure.lean
@@ -9,8 +9,21 @@ def is_CF {T : Type} (L : language T) :=
 ∃ g : CF_grammar T, CF_language g = L
 
 
+
+lemma CF_derives_reflexive {T : Type} {g : CF_grammar T} {w : list (symbol T g.nt)} : CF_derives g w w :=
+begin
+  fconstructor,
+end
+
+lemma list_three_parts {T₁ T₂ : Type} {x y z : list T₁} {f : T₁ → T₂} : 
+  list.map f (x ++ y ++ z) = (list.map f x) ++ (list.map f y) ++ (list.map f z) :=
+begin
+  simp,
+end
+
+
 theorem CF_under_union {T : Type} (L₁ : language T) (L₂ : language T) :
-is_CF L₁ ∧ is_CF L₂ → is_CF (L₁ + L₂) :=
+  ((is_CF L₁) ∧ (is_CF L₂))  →  (is_CF (L₁ + L₂)) :=
 begin
   intro input,
   cases input with cf₁ cf₂,
@@ -21,33 +34,38 @@ begin
   let N₁ : Type := g₁.nt,
   let N₂ : Type := g₂.nt,
   let N : Type := option (N₁ ⊕ N₂),
+
+  let convert_sTN₁_sTN : (symbol T N₁) → (symbol T N) :=
+    λ s, match s with
+      | symbol.terminal st := (symbol.terminal st)
+      | symbol.nonterminal snt := (symbol.nonterminal (some (sum.inl snt)))
+    end,
+  let convert_sTN₂_sTN : (symbol T N₂) → (symbol T N) :=
+    λ s, match s with
+      | symbol.terminal st := (symbol.terminal st)
+      | symbol.nonterminal snt := (symbol.nonterminal (some (sum.inr snt)))
+    end,
+
+  let convert_lsTN₁_lsTN : list (symbol T N₁) → list (symbol T N) :=
+    list.map convert_sTN₁_sTN,
+  let convert_lsTN₂_lsTN : list (symbol T N₂) → list (symbol T N) :=
+    list.map convert_sTN₂_sTN,
+
+  let convert_rule₁_rule : (N₁ × (list (symbol T N₁))) → (N × (list (symbol T N))) :=
+    λ r, (some (sum.inl (prod.fst r)), convert_lsTN₁_lsTN (prod.snd r)),
+  let convert_rule₂_rule : (N₂ × (list (symbol T N₂))) → (N × (list (symbol T N))) :=
+    λ r, (some (sum.inr (prod.fst r)), convert_lsTN₂_lsTN (prod.snd r)),
   
   let rules₁ : list (N × (list (symbol T N))) :=
-    list.map 
-      (λ x, (
-        (some (sum.inl (prod.fst x))),
-        (list.map (λ elem : (symbol T N₁), match elem with
-          | symbol.terminal st := (symbol.terminal st)
-          | symbol.nonterminal snt := (symbol.nonterminal (some (sum.inl snt)))
-        end) (prod.snd x))
-      ))
-      g₁.rules,
-  
+    list.map convert_rule₁_rule g₁.rules,
   let rules₂ : list (N × (list (symbol T N))) :=
-    list.map 
-      (λ x, (
-        (some (sum.inr (prod.fst x))),
-        (list.map (λ elem : (symbol T N₂), match elem with
-          | symbol.terminal st := (symbol.terminal st)
-          | symbol.nonterminal snt := (symbol.nonterminal (some (sum.inr snt)))
-        end) (prod.snd x))
-      ))
-      g₂.rules,
+    list.map convert_rule₂_rule g₂.rules,
 
   let g := CF_grammar.mk N none (
     (none, [symbol.nonterminal (some (sum.inl (g₁.initial)))]) ::
     (none, [symbol.nonterminal (some (sum.inr (g₂.initial)))]) ::
-    rules₁ ++ rules₂),
+    (rules₁ ++ rules₂)
+  ),
   use g,
   
   apply set.eq_of_subset_of_subset,
@@ -66,17 +84,6 @@ begin
   
   cases h with case₁ case₂,
   {
-    have deri_orig : CF_derives g₁ [symbol.nonterminal g₁.initial] (list.map symbol.terminal w),
-    {
-      finish,
-    },
-
-    have deri_rest : CF_derives g [symbol.nonterminal (some (sum.inl g₁.initial))] (list.map symbol.terminal w),
-    {
-      dsimp at *,
-      sorry,
-    },
-
     have deri_start : CF_derives g [symbol.nonterminal none] [symbol.nonterminal (some (sum.inl g₁.initial))],
     {
       fconstructor,
@@ -89,6 +96,80 @@ begin
       simp,
     },
 
+    have deri_rest : CF_derives g [symbol.nonterminal (some (sum.inl g₁.initial))] (list.map symbol.terminal w),
+    {
+      have deri_orig : CF_derives g₁ [symbol.nonterminal g₁.initial] (list.map symbol.terminal w),
+      {
+        finish,
+      },
+      dsimp at *,
+      have deri_step : ∀ input output : list (symbol T N₁),
+                        CF_transforms g₁ input output → 
+                        CF_transforms g (convert_lsTN₁_lsTN input) (convert_lsTN₁_lsTN output),
+      {
+        intros inpu outpu ass,
+        cases ass with rul foo,
+        cases foo with bel bar,
+        cases bar with v baz,
+        cases baz with w hyp,
+        cases hyp with befo afte,
+
+        use convert_rule₁_rule rul,
+        split,
+        {
+          change convert_rule₁_rule rul ∈ (
+            (none, [symbol.nonterminal (some (sum.inl (g₁.initial)))]) ::
+            (none, [symbol.nonterminal (some (sum.inr (g₂.initial)))]) ::
+            (list.map convert_rule₁_rule g₁.rules) ++ rules₂),
+          have isthere : convert_rule₁_rule rul ∈ list.map convert_rule₁_rule g₁.rules :=
+            list.mem_map_of_mem convert_rule₁_rule bel,
+          finish,
+        },
+        use convert_lsTN₁_lsTN v,
+        use convert_lsTN₁_lsTN w,
+        split,
+        {
+          rw congr_arg convert_lsTN₁_lsTN befo,
+          apply list_three_parts,
+        },
+        {
+          change convert_lsTN₁_lsTN outpu = convert_lsTN₁_lsTN v ++ convert_lsTN₁_lsTN rul.snd ++ convert_lsTN₁_lsTN w,
+          rw congr_arg convert_lsTN₁_lsTN afte,
+          apply list_three_parts,
+        },
+      },
+      have deri_more : ∀ input output : list (symbol T N₁),
+                         CF_derives g₁ input output →
+                         CF_derives g (convert_lsTN₁_lsTN input) (convert_lsTN₁_lsTN output),
+      {
+        intros inp outp ass,
+        induction ass with u v ih₁ orig ih,
+        {
+          apply CF_derives_reflexive,
+        },
+        apply @trans _ (CF_derives g) (is_trans.mk relation.transitive_refl_trans_gen),
+          finish,
+        fconstructor,
+          exact (convert_lsTN₁_lsTN u),
+        refl,
+        exact deri_step u v orig,
+      },
+      have beginning : [symbol.nonterminal (some (sum.inl g₁.initial))]
+                       = convert_lsTN₁_lsTN [symbol.nonterminal g₁.initial],
+      {
+        finish,
+      },
+      have ending : (list.map symbol.terminal w)
+                    = convert_lsTN₁_lsTN (list.map symbol.terminal w),
+      {
+        ext1,
+        simp,
+      },
+      rw beginning,
+      rw ending,
+      exact deri_more [symbol.nonterminal g₁.initial] (list.map symbol.terminal w) deri_orig,
+    },
+
     unfold CF_language,
     change CF_generates_str g (list.map symbol.terminal w),
     unfold CF_generates_str,
@@ -98,7 +179,7 @@ begin
     exact deri_rest,
   },
 
-  sorry, -- prove case₂ symmetrically
+  sorry, -- TODO prove `case₂` symmetrically
 end
 
 
