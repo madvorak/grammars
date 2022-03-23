68f7ae3e063fcd62191f780a502cf47aab21c731
madvorak <dvorakmartinbridge@seznam.cz>
2022-03-23 01:31:58 +0100

    lifting and sinking grammars proved

--- src/context_free/closure_properties/binary/CF_concatenation_CF.lean
+++ src/context_free/closure_properties/binary/CF_concatenation_CF.lean
@@ -1,4 +1,5 @@
 import context_free.cfg
+import context_free.closure_properties.binary.CF_union_CF
 import tactic
 
 
@@ -9,25 +10,34 @@ def lift_symbol {N₁ N : Type} (lift_N : N₁ → N) : symbol T N₁ → symbol
 | (symbol.terminal ter) := symbol.terminal ter
 | (symbol.nonterminal nonter) := symbol.nonterminal (lift_N nonter)
 
+def lift_string {N₁ N : Type} (lift_N : N₁ → N) :
+  list (symbol T N₁) → list (symbol T N) :=
+list.map (lift_symbol lift_N)
+
+def lift_rule {N₁ N : Type} (lift_N : N₁ → N) :
+  N₁ × (list (symbol T N₁)) → N × (list (symbol T N)) :=
+λ r, (lift_N r.fst, lift_string lift_N r.snd)
+
 def lift_grammar (g₁ : CF_grammar T) {N : Type} (lift_N : g₁.nt → N)
-                 (lift_N_inj : function.injective lift_N) : CF_grammar T :=
-CF_grammar.mk N (lift_N g₁.initial) (list.map (λ r : g₁.nt × (list (symbol T g₁.nt)), 
-  (lift_N r.fst, list.map (lift_symbol lift_N) r.snd)) g₁.rules)
+                 (lift_N_inj : function.injective lift_N) :
+  CF_grammar T :=
+CF_grammar.mk N (lift_N g₁.initial) (list.map (lift_rule lift_N) g₁.rules)
+
 
 lemma lift_tran (g₁ : CF_grammar T) {N : Type} (lift_N : g₁.nt → N)
                 (lift_N_inj : function.injective lift_N)
                 (input output : list (symbol T g₁.nt))
                 (hyp : CF_transforms g₁ input output) :
   CF_transforms (lift_grammar g₁ lift_N lift_N_inj)
-                (list.map (lift_symbol lift_N) input)
-                (list.map (lift_symbol lift_N) output) :=
+                (lift_string lift_N input)
+                (lift_string lift_N output) :=
 begin
   rcases hyp with ⟨ rule, rule_in, v, w, ass ⟩,
-  use (lift_N rule.fst, list.map (lift_symbol lift_N) rule.snd),
+  use lift_rule lift_N rule,
   split,
   {
     delta lift_grammar,
-    simp only [list.mem_map, prod.mk.inj_iff],
+    simp only [lift_grammar, list.mem_map, prod.mk.inj_iff],
     use rule,
     split,
     {
@@ -37,11 +47,14 @@ begin
     refl,
   },
   simp,
-  use list.map (lift_symbol lift_N) v,
-  use list.map (lift_symbol lift_N) w,
+  use lift_string lift_N v,
+  use lift_string lift_N w,
   cases ass with bef aft,
   split;
-  finish,
+  {
+    unfold lift_string,
+    finish,
+  },
 end
 
 lemma lift_deri (g₁ : CF_grammar T) {N : Type} (lift_N : g₁.nt → N)
@@ -49,8 +62,8 @@ lemma lift_deri (g₁ : CF_grammar T) {N : Type} (lift_N : g₁.nt → N)
                 (input output : list (symbol T g₁.nt))
                 (hyp : CF_derives g₁ input output) :
   CF_derives (lift_grammar g₁ lift_N lift_N_inj)
-             (list.map (lift_symbol lift_N) input)
-             (list.map (lift_symbol lift_N) output) :=
+             (lift_string lift_N input)
+             (lift_string lift_N output) :=
 begin
   induction hyp with u v trash orig ih,
   {
@@ -61,9 +74,28 @@ begin
     exact ih,
   },
   rcases orig with ⟨ rul, rin, w₁, w₂, bef, aft ⟩,
-  --use lift_grammar rul lift_N lift_N_inj,
-
-  sorry,
+  use lift_rule lift_N rul,
+  split,
+  {
+    change lift_rule lift_N rul ∈ list.map (lift_rule lift_N) g₁.rules,
+    exact list.mem_map_of_mem (lift_rule lift_N) rin,
+  },
+  use lift_string lift_N w₁,
+  use lift_string lift_N w₂,
+  split,
+  {
+    rw bef,
+    unfold lift_string,
+    rw list_map_append_append,
+    rw list.map_singleton,
+    refl,
+  },
+  {
+    rw aft,
+    unfold lift_string,
+    rw list_map_append_append,
+    refl,
+  },
 end
 
 lemma lift_generates (g₁ : CF_grammar T) {N : Type} (lift_N : g₁.nt → N)
@@ -75,12 +107,28 @@ begin
   let g := (lift_grammar g₁ lift_N lift_N_inj),
   change CF_derives g [symbol.nonterminal g.initial] (list.map symbol.terminal w),
   have h₁ := lift_deri g₁ lift_N lift_N_inj [symbol.nonterminal g₁.initial] (list.map symbol.terminal w) hyp,
+  unfold lift_string at h₁,
   rw list.map_singleton at h₁,
   rw lift_symbol at h₁,
   simp at h₁,
   exact h₁,
 end
 
+
+lemma lift_sink_eq_id {N N₁ : Type} {lift_N : N₁ → N} {sink_N : N → N₁}
+                      (lift_N_sink : ∀ n₁ : N₁, sink_N (lift_N n₁) = n₁)
+                      (T : Type) :
+  @lift_symbol T N N₁ sink_N ∘ @lift_symbol T N₁ N lift_N = id :=
+begin
+  ext1,
+  cases x,
+  {
+    refl,
+  },
+  delta lift_symbol,
+  dsimp,
+  rw lift_N_sink,
+end
 
 lemma sink_tran (g₁ : CF_grammar T) {N : Type} 
                 (lift_N : g₁.nt → N) (sink_N : N → g₁.nt)
@@ -90,10 +138,49 @@ lemma sink_tran (g₁ : CF_grammar T) {N : Type}
                 (hyp : CF_transforms (lift_grammar g₁ lift_N lift_N_inj) input output) :
   CF_transforms g₁ (list.map (lift_symbol sink_N) input) (list.map (lift_symbol sink_N) output) :=
 begin
-  let sinked_grammar := lift_grammar (lift_grammar g₁ lift_N lift_N_inj) sink_N sorry,
   rcases hyp with ⟨ rule, rule_in, v, w, bef, aft ⟩,
-
-  sorry,
+  unfold CF_transforms,
+  let sinked_rule := lift_rule sink_N rule,
+  change rule ∈ (list.map (lift_rule lift_N) g₁.rules) at rule_in,
+  use sinked_rule,
+  split,
+  {
+    change lift_rule sink_N rule ∈ g₁.rules,
+    rw list.mem_map at rule_in,
+    rcases rule_in with ⟨ the_rule, the_in, the_hyp ⟩,
+    rw ← the_hyp,
+    convert the_in,
+    ext1,
+    {
+      unfold lift_rule,
+      dsimp,
+      rw lift_N_sink,
+    },
+    {
+      unfold lift_rule,
+      unfold lift_string,
+      dsimp,
+      rw list.map_map,
+      rw lift_sink_eq_id lift_N_sink T,
+      exact the_rule.snd.map_id,
+    },
+  },
+  use lift_string sink_N v,
+  use lift_string sink_N w,
+  split,
+  {
+    rw bef,
+    rw lift_string,
+    rw list_map_append_append,
+    rw list.map_singleton,
+    refl,
+  },
+  {
+    rw aft,
+    rw lift_string,
+    rw list_map_append_append,
+    refl,
+  },
 end
 
 lemma sink_deri (g₁ : CF_grammar T) {N : Type} 
@@ -104,12 +191,81 @@ lemma sink_deri (g₁ : CF_grammar T) {N : Type}
                 (hyp : CF_derives (lift_grammar g₁ lift_N lift_N_inj) input output) :
   CF_derives g₁ (list.map (lift_symbol sink_N) input) (list.map (lift_symbol sink_N) output) :=
 begin
-
-  sorry,
+  induction hyp with u v trash orig ih,
+  {
+    apply CF_deri_self,
+  },
+  apply CF_deri_of_deri_tran,
+  {
+    exact ih,
+  },
+  rcases orig with ⟨ rul, rin, w₁, w₂, bef, aft ⟩,
+  use lift_rule sink_N rul,
+  split,
+  {
+    change rul ∈ list.map (lift_rule lift_N) g₁.rules at rin,
+    have trick := list.mem_map_of_mem (lift_rule sink_N) rin,
+    convert trick,
+    ext1,
+    rw list.nth_map,
+    rw list.nth_map,
+    cases g₁.rules.nth n,
+    {
+      refl,
+    },
+    rw option.map_map,
+    unfold lift_rule,
+    dsimp,
+    rw lift_N_sink,
+    unfold lift_string,
+    rw list.map_map,
+    cases val,
+    rw lift_sink_eq_id lift_N_sink T,
+    rw list.map_id,
+  },
+  use lift_string sink_N w₁,
+  use lift_string sink_N w₂,
+  split,
+  {
+    rw bef,
+    unfold lift_string,
+    rw list_map_append_append,
+    rw list.map_singleton,
+    refl,
+  },
+  {
+    rw aft,
+    unfold lift_string,
+    rw list_map_append_append,
+    refl,
+  },
 end
 
+lemma sink_generates (g₁ : CF_grammar T) {N : Type}
+                     (lift_N : g₁.nt → N) (sink_N : N → g₁.nt)
+                     (lift_N_inj : function.injective lift_N)
+                     (lift_N_sink : ∀ n₁ : g₁.nt, sink_N (lift_N n₁) = n₁)
+                     (w : list T)
+                     (hyp : CF_generates (lift_grammar g₁ lift_N lift_N_inj) w) :
+  CF_generates g₁ w :=
+begin
+  let g := (lift_grammar g₁ lift_N lift_N_inj),
+  change CF_derives g [symbol.nonterminal g.initial] (list.map symbol.terminal w) at hyp,
+  change CF_derives g₁ [symbol.nonterminal g₁.initial] (list.map symbol.terminal w),
+  have h₁ := sink_deri g₁ lift_N sink_N lift_N_inj lift_N_sink
+                       [symbol.nonterminal g.initial]
+                       (list.map symbol.terminal w)
+                       hyp,
+  rw list.map_singleton at h₁,
+  rw lift_symbol at h₁,
+  simp at h₁,
+  convert h₁,
+  change g₁.initial = sink_N (lift_N g₁.initial),
+  rw lift_N_sink,
+end
 
-
+#check @lift_generates T
+#check @sink_generates T
 
 
 private def sTN_of_sTN₁ {g₁ g₂ : CF_grammar T} : (symbol T g₁.nt) → (symbol T (option (g₁.nt ⊕ g₂.nt)))
@@ -140,17 +296,6 @@ private def combined_grammar (gₗ gᵣ : CF_grammar T) :
   CF_grammar T :=
 CF_grammar.mk (option (gₗ.nt ⊕ gᵣ.nt)) none
   (((list.map rule_of_rule₁ gₗ.rules) ++ (list.map rule_of_rule₂ gᵣ.rules)))
-
-
-
-private lemma deri₁_step (g₁ g₂ : CF_grammar T)
-                         (input output : list (symbol T g₁.nt))
-                         (hyp : CF_transforms g₁ input output) :
-  CF_transforms (combined_grammar g₁ g₂) (lsTN_of_lsTN₁ input) (lsTN_of_lsTN₁ output) :=
-begin
-  --exact lift_tran (lift_grammar g₁ (λ n₁, option.some (sum.inl n₁)) sorry) input output sorry,
-  sorry,
-end
 
 
 /-- The class of context-free languages is closed under concatenation. -/
