588d91a9ede13dadf9bd7ef6884a1dceb7976dd2
madvorak <dvorakmartinbridge@seznam.cz>
2022-09-19 19:44:50 +0200

    alternative definition of unrestricted grammars

---
+++ src/unrestricted/closure_properties/binary/RE_intersection_RE.lean
@@ -0,0 +1,74 @@
+import unrestricted.closure_properties.binary.RE_concatenation_RE
+
+
+def super_nonterminal (T N₁ N₂ : Type) : Type :=
+(option (N₁ ⊕ T)) × ((N₂ ⊕ T) ⊕ (((N₂ ⊕ T) × (N₂ ⊕ T)) ⊕ (fin 2)))
+
+
+def rule_step_right {T N₁ N₂ : Type} (s₁ s₂ : option (N₁ ⊕ T)) (x y z : N₂ ⊕ T) :
+  frule T (super_nonterminal T N₁ N₂) :=
+frule.mk
+  [symbol.nonterminal (s₁, sum.inr (sum.inl (x, y))), symbol.nonterminal (s₂, sum.inl z)]
+  (s₁, sum.inr (sum.inl (x, y)))
+  (list.mem_cons_self _ _)
+  [symbol.nonterminal (s₁, sum.inl x), symbol.nonterminal (s₂, sum.inr (sum.inl (y, z)))]
+
+def rule_step_right_end {T N₁ N₂ : Type} (s₁ s₂ : option (N₁ ⊕ T)) (x y : N₂ ⊕ T) :
+  frule T (super_nonterminal T N₁ N₂) :=
+frule.mk
+  [symbol.nonterminal (s₁, sum.inr (sum.inl (x, y))), symbol.nonterminal (s₂, sum.inr (sum.inr 0))]
+  (s₁, sum.inr (sum.inl (x, y)))
+  (list.mem_cons_self _ _)
+  [symbol.nonterminal (s₁, sum.inl x), symbol.nonterminal (s₂, sum.inl y)]
+
+def rule_step_left {T N₁ N₂ : Type} (s₁ s₂ : option (N₁ ⊕ T)) (x : N₂ ⊕ T) :
+  frule T (super_nonterminal T N₁ N₂) :=
+frule.mk
+  [symbol.nonterminal (s₁, sum.inr (sum.inr 1)), symbol.nonterminal (s₂, sum.inl x)]
+  (s₁, sum.inr (sum.inr 1))
+  (list.mem_cons_self _ _)
+  [symbol.nonterminal (s₁, sum.inl x), symbol.nonterminal (s₂, sum.inr (sum.inr 1))]
+
+def rule_step_left_end {T N₁ N₂ : Type} (s₁ s₂ : option (N₁ ⊕ T)) :
+  frule T (super_nonterminal T N₁ N₂) :=
+frule.mk
+  [symbol.nonterminal (s₁, sum.inr (sum.inr 1)), symbol.nonterminal (s₂, sum.inr (sum.inr 0))]
+  (s₁, sum.inr (sum.inr 1))
+  (list.mem_cons_self _ _)
+  [symbol.nonterminal (s₁, sum.inr (sum.inr 0)), symbol.nonterminal (s₂, sum.inr (sum.inr 0))]
+
+def rule_finalize_terminal {T N₁ N₂ : Type} (t : T) :
+  frule T (super_nonterminal T N₁ N₂) :=
+frule.mk
+  [symbol.nonterminal (some (sum.inr t), sum.inl (sum.inr t))]
+  (some (sum.inr t), sum.inl (sum.inr t))
+  (list.mem_singleton_self _)
+  [symbol.terminal t]
+
+def rule_delete_empty_empty {T N₁ N₂ : Type} :
+  frule T (super_nonterminal T N₁ N₂) :=
+frule.mk
+  [symbol.nonterminal (none, sum.inr (sum.inr 0))]
+  (none, sum.inr (sum.inr 0))
+  (list.mem_singleton_self _)
+  []
+
+
+def all_used_terminals_f {T : Type} (g : frammar T) : list T :=
+list.filter_map as_terminal (list.join (list.map frule.output_string g.rules))
+
+def rules_finalize_terminal {T : Type} (g₁ g₂ : frammar T) :
+  list (frule T (super_nonterminal T g₁.nt g₂.nt)) :=
+list.map (λ t, rule_finalize_terminal t) (all_used_terminals_f g₁ ++ all_used_terminals_f g₂)
+-- intersection of `all_used_terminals_f g₁` and `all_used_terminals_f g₂` should be sufficient
+-- maybe just use one of them (whichever is more convenient for the proof at the end)
+
+def all_used_nonterminals_f {T : Type} (g : frammar T) : list g.nt :=
+sorry
+
+def rules_step_left_end {T : Type} (g₁ g₂ : frammar T) :
+  list (frule T (super_nonterminal T g₁.nt g₂.nt)) :=
+let z := option.none ::
+  (list.map (option.some ∘ sum.inl) (all_used_nonterminals_f g₁)) ++
+  (list.map (option.some ∘ sum.inr) (all_used_terminals_f g₁))
+in list.join (z.map (λ s₁, z.map (λ s₂, rule_step_left_end s₁ s₂)))
--- src/unrestricted/grammar.lean
+++ src/unrestricted/grammar.lean
@@ -19,11 +19,22 @@ structure grule (τ : Type) (ν : Type) :=
 (input_R : list (symbol τ ν))
 (output_string : list (symbol τ ν))
 
+structure frule (τ : Type) (ν : Type) :=
+(input_string : list (symbol τ ν))
+(marked_nt : ν)
+(contains_nt : symbol.nonterminal marked_nt ∈ input_string)
+(output_string : list (symbol τ ν))
+
 /-- Grammar (general) that generates words over the alphabet `termi` (a type of terminals). -/
 structure grammar (termi : Type) :=
 (nt : Type)                     -- type of nonterminals
 (initial : nt)                  -- initial symbol
 (rules : list (grule termi nt)) -- rewriting rules
+
+structure frammar (termi : Type) :=
+(nt : Type)                     -- type of nonterminals
+(initial : nt)                  -- initial symbol
+(rules : list (frule termi nt)) -- rewriting rules
 
 
 variables {T : Type}
@@ -36,22 +47,129 @@ def grammar_transforms (g : grammar T) (w₁ w₂ : list (symbol T g.nt)) : Prop
     (w₁ = u ++ r.input_L ++ [symbol.nonterminal r.input_N] ++ r.input_R ++ v)
     (w₂ = u ++ r.output_string ++ v)
 
+def frammar_transforms (g : frammar T) (w₁ w₂ : list (symbol T g.nt)) : Prop :=
+∃ r : frule T g.nt,
+  r ∈ g.rules ∧
+  ∃ u v : list (symbol T g.nt), and
+    (w₁ = u ++ r.input_string ++ v)
+    (w₂ = u ++ r.output_string ++ v)
+
 /-- Any number of steps of grammatical transformation; reflexive+transitive closure of `grammar_transforms`. -/
 def grammar_derives (g : grammar T) : list (symbol T g.nt) → list (symbol T g.nt) → Prop :=
 relation.refl_trans_gen (grammar_transforms g)
 
+def frammar_derives (g : frammar T) : list (symbol T g.nt) → list (symbol T g.nt) → Prop :=
+relation.refl_trans_gen (frammar_transforms g)
+
 def grammar_generates (g : grammar T) (w : list T) : Prop :=
 grammar_derives g [symbol.nonterminal g.initial] (list.map symbol.terminal w)
+
+def frammar_generates (g : frammar T) (w : list T) : Prop :=
+frammar_derives g [symbol.nonterminal g.initial] (list.map symbol.terminal w)
 
 /-- Returns the set of words (lists of terminals) that can be derived from the initial nonterminal. -/
 def grammar_language (g : grammar T) : language T :=
 set_of (grammar_generates g)
+
+def frammar_language (g : frammar T) : language T :=
+set_of (frammar_generates g)
 
 /-- Predicate "is recursively-enumerable"; defined by an existence of a grammar for the given language. -/
 def is_RE (L : language T) : Prop :=
 ∃ g : grammar T, grammar_language g = L
 
 end grammar_definitions
+
+
+section grammar_frammar
+
+variables {T : Type}
+
+def frule_of_grule {N : Type} (r : grule T N) : frule T N :=
+frule.mk
+  (r.input_L ++ [symbol.nonterminal r.input_N] ++ r.input_R)
+  r.input_N
+  (by {
+    apply list.mem_append_left,
+    apply list.mem_append_right,
+    apply list.mem_singleton_self,
+  })
+  r.output_string
+
+def grule_of_frule {N : Type} (r : frule T N) : grule T N :=
+let index := /-list.index_of r.marked_nt r.input_string-/ 42
+in grule.mk -- TODO
+  (list.take index r.input_string)
+  r.marked_nt
+  (list.drop (index + 1) r.input_string)
+  r.output_string
+
+def frammar_of_grammar (g : grammar T) : frammar T :=
+frammar.mk g.nt g.initial (list.map frule_of_grule g.rules)
+
+def grammar_of_frammar (g : frammar T) : grammar T :=
+grammar.mk g.nt g.initial (list.map grule_of_frule g.rules)
+
+lemma frammar_transforms_of_grammar_transforms (g : grammar T) (w₁ w₂ : list (symbol T g.nt)) :
+  grammar_transforms g w₁ w₂  ↔  frammar_transforms (frammar_of_grammar g) w₁ w₂  :=
+begin
+  split,
+  {
+    rintro ⟨r, rin, u, v, bef, aft⟩,
+    use frule_of_grule r,
+    split,
+    {
+      change frule_of_grule r ∈ (list.map frule_of_grule g.rules),
+      rw list.mem_map,
+      use r,
+      exact ⟨rin, rfl⟩,
+    },
+    use u,
+    use v,
+    split,
+    {
+      repeat {
+        rw list.append_assoc at bef,
+      },
+      rw ←list.append_assoc [symbol.nonterminal r.input_N] _ _ at bef,
+      rw ←list.append_assoc r.input_L _ _ at bef,
+      rw ←list.append_assoc r.input_L _ _ at bef,
+      rw ←list.append_assoc at bef,
+      exact bef,
+    },
+    {
+      exact aft,
+    },
+  },
+  {
+    rintro ⟨r, rin, u, v, bef, aft⟩,
+    use grule_of_frule r,
+    sorry,
+  },
+end
+
+lemma frammar_transforms_of_grammar_transforms' (g : grammar T) :
+  grammar_transforms g = frammar_transforms (frammar_of_grammar g) :=
+begin
+  ext w₁ w₂,
+  exact frammar_transforms_of_grammar_transforms g w₁ w₂,
+end
+
+lemma frammar_derives_of_grammar_derives' (g : grammar T) :
+  grammar_derives g = frammar_derives (frammar_of_grammar g) :=
+begin
+  unfold grammar_derives,
+  rw frammar_transforms_of_grammar_transforms',
+  refl,
+end
+
+lemma frammar_derives_of_grammar_derives (g : grammar T) (v w : list (symbol T g.nt)) :
+  grammar_derives g v w  ↔  frammar_derives (frammar_of_grammar g) v w  :=
+begin
+  rw frammar_derives_of_grammar_derives' g,
+end
+
+end grammar_frammar
 
 
 section grammar_utilities
