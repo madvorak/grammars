2f2be1d1be907ba0e54289eb21db0cafab781dea
madvorak <dvorakmartinbridge@seznam.cz>
2022-10-18 08:50:45 +0200

    grammar for Kleene star

--- src/unrestricted/closure_properties/unary/star_RE.lean
+++ src/unrestricted/closure_properties/unary/star_RE.lean
@@ -17,20 +17,40 @@ symbol T (nn N)
 
 variables {T : Type}
 
-private def wrap_symbol {N : Type} : symbol T N → ns T N
+private def wrap_sym {N : Type} : symbol T N → ns T N
 | (symbol.terminal t)    := symbol.terminal t
 | (symbol.nonterminal n) := symbol.nonterminal (sum.inl n)
 
+private def wrap_gr {N : Type} (r : grule T N) : grule T (nn N) :=
+grule.mk
+  (list.map wrap_sym r.input_L)
+  (sum.inl r.input_N)
+  (list.map wrap_sym r.input_R)
+  (list.map wrap_sym r.output_string)
 
+private def rules_for_scanning_terminals (g : grammar T) : list (grule T (nn g.nt)) :=
+list.map (λ t, -- `Rt → tR`
+    grule.mk [] (sum.inr 2) [symbol.terminal t] [symbol.terminal t, symbol.nonterminal (sum.inr 2)]
+  ) (all_used_terminals g)
+
+-- based on `/informal/KleeneStar.pdf`
 private def star_grammar (g : grammar T) : grammar T :=
 grammar.mk
   (nn g.nt)
   (sum.inr 0)
-  ((grule.mk [] (sum.inr 0) [] [ -- `Z → ZS#`
-    symbol.nonterminal (sum.inr 0),
-    symbol.nonterminal (sum.inl g.initial),
-    symbol.nonterminal (sum.inr 1)]
-  ) :: sorry)
+  (
+    (grule.mk [] (sum.inr 0) [] [ -- `Z → ZS#`
+      symbol.nonterminal (sum.inr 0),
+      symbol.nonterminal (sum.inl g.initial),
+      symbol.nonterminal (sum.inr 1)]
+    ) :: (grule.mk [] (sum.inr 0) [] [ -- `Z → R#`
+      symbol.nonterminal (sum.inr 2),
+      symbol.nonterminal (sum.inr 1)]
+    ) :: (grule.mk [] (sum.inr 2) [symbol.nonterminal (sum.inr 1)] [ -- `R# → R`
+      symbol.nonterminal (sum.inr 2)]
+    ) :: (grule.mk [] (sum.inr 0) [] [ -- `R# → ∅`
+    ]) :: list.map wrap_gr g.rules ++ 
+    rules_for_scanning_terminals g)
 
 
 /-- The class of recursively-enumerable languages is closed under the Kleene star. -/
