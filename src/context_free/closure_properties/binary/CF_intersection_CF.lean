f3208060b064e8da6296ec5d54db1748f0937d72
madvorak <dvorakmartinbridge@seznam.cz>
2022-12-20 14:59:19 +0100

    tiny golf in CF_intersection_CF

--- src/context_free/closure_properties/binary/CF_intersection_CF.lean
+++ src/context_free/closure_properties/binary/CF_intersection_CF.lean
@@ -1442,21 +1442,16 @@ theorem nnyCF_of_CF_i_CF : ¬ (∀ T : Type, ∀ L₁ : language T, ∀ L₂ : l
     is_CF L₁  ∧  is_CF L₂   →   is_CF (L₁ ⊓ L₂)
 ) :=
 begin
-  by_contradiction,
-  specialize h (fin 3) lang_eq_any lang_any_eq,
-  specialize h (and.intro CF_lang_eq_any CF_lang_any_eq),
+  by_contradiction contra,
+  specialize contra (fin 3) lang_eq_any lang_any_eq ⟨CF_lang_eq_any, CF_lang_any_eq⟩,
+  apply notCF_lang_eq_eq,
+  convert contra,
 
-  have intersection : lang_eq_any ⊓ lang_any_eq = lang_eq_eq,
+  apply set.eq_of_subset_of_subset,
   {
-    apply set.eq_of_subset_of_subset,
-    {
-      apply lang_eq_eq_of_intersection,
-    },
-    {
-      apply intersection_of_lang_eq_eq,
-    },
+    apply intersection_of_lang_eq_eq,
   },
-  
-  rw intersection at h,
-  exact notCF_lang_eq_eq h,
+  {
+    apply lang_eq_eq_of_intersection,
+  },
 end
