eeca448ca5b22a8a53683e9464d81ec9438a9d78
madvorak <dvorakmartinbridge@seznam.cz>
2022-12-20 14:20:10 +0100

    golfed complement_CF

--- src/context_free/closure_properties/unary/complement_CF.lean
+++ src/context_free/closure_properties/unary/complement_CF.lean
@@ -12,46 +12,9 @@ begin
   push_neg at nny,
   rcases nny with ⟨T, L₁, L₂, ⟨hL₁, hL₂⟩, hyp_neg⟩,
   specialize h T,
-  have h₁ := h L₁ hL₁,
-  have h₂ := h L₂ hL₂,
-  have hu := CF_of_CF_u_CF (L₁ᶜ) (L₂ᶜ) (and.intro h₁ h₂),
-  
-  -- the rest of the proof is just De Morgan with extra steps
-  have hnu := h ((L₁ᶜ) + (L₂ᶜ)),
-  have contra := hnu hu,
+  have hu := CF_of_CF_u_CF (L₁ᶜ) (L₂ᶜ) ⟨h L₁ hL₁, h L₂ hL₂⟩,
+  have contra := h (L₁ᶜ + L₂ᶜ) hu,
   apply hyp_neg,
-  convert contra,
-  apply set.eq_of_subset_of_subset;
-  intro v;
-  intro hv,
-  {
-    cases hv with hv₁ hv₂,
-    intro hvw,
-    cases hvw,
-    {
-      exact hvw hv₁,
-    },
-    {
-      exact hvw hv₂,
-    },
-  },
-  {
-    by_cases c₁ : v ∈ L₁,
-    by_cases c₂ : v ∈ L₂,
-    {
-      exact and.intro c₁ c₂,
-    },
-    {
-      exfalso,
-      apply hv,
-      right,
-      exact c₂,
-    },
-    {
-      exfalso,
-      apply hv,
-      left,
-      exact c₁,
-    },
-  },
+  -- golfed by Eric Wieser
+  rwa [language.add_def, set.compl_union, compl_compl, compl_compl] at contra,
 end
